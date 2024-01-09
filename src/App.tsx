8f70e717ac7f850fb3d3b2439c7740af2524a11f
Admin <dev.besu@gmail.com>
2024-01-09 21:18:57 +0100

    add images, create image component & add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Image from './Image';
+import king from './assets/data/equipment/barbarian_king.json';
+
 const App = () => {
-  return <div>App</div>;
+  console.log(king);
+
+  const test = import('./assets/img/equipment/Barbarian_Puppet.webp');
+
+  console.log(test);
+
+  return (
+    <div className="font-supercellMagic">
+      <div>Hejsan vafan</div>
+      <Image url={`./assets/img/equipment/${king[0].img}`} />
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      supercellMagic: ['supercell-magic', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],

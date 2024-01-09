305c8910ad5190b28ca2d5cdc024aaa661f2b8d6
Admin <dev.besu@gmail.com>
2024-01-09 21:24:51 +0100

    feat: move images to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,10 @@ import king from './assets/data/equipment/barbarian_king.json';
 const App = () => {
   console.log(king);
 
-  const test = import('./assets/img/equipment/Barbarian_Puppet.webp');
-
-  console.log(test);
-
   return (
     <div className="font-supercellMagic">
       <div>Hejsan vafan</div>
-      <Image url={`./assets/img/equipment/${king[0].img}`} />
+      <Image url={`../public/img/equipment/${king[0].img}`} />
     </div>
   );
 };

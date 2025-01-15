8a2f3391f7a83603a15183755e07f0af20fcb8df
sndell <dev.besu@gmail.com>
2025-01-15 19:09:12 +0100

    chore: update manifest to make app installable

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,19 +2,24 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Clash of Clans Ore Calculator - Max Your Equipment',
+    name: 'Clash of Clans Ore Calculator',
     short_name: 'Ore Calculator',
     description:
-      'Calculate how many ores are remaining until you max all of your equipment in Clash of Clans. A simple but powerful tool.',
+      'Calculate how many ores you need to max all equipments, a single equipment or how many ores you can get per week',
     start_url: '/',
     display: 'standalone',
-    background_color: '#000',
-    theme_color: '#000',
+    background_color: '#000000',
+    theme_color: '#000000',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: '30x32',
-        type: 'image/x-icon',
+        src: '/images/ores/Starry_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/ores/Starry_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
     ],
   };

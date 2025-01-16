310702ececd793ced819cadf785e31678382b9db
sndell <dev.besu@gmail.com>
2025-01-16 20:58:47 +0100

    chore: remove unused stuff

--- src/features/max-calculator/components/HeroCard.tsx
+++ src/features/max-calculator/components/HeroCard.tsx
@@ -2,7 +2,7 @@ import { HeroEquipment } from './HeroEquipment';
 
 export const HeroCard = ({ name, equipment }: { name: string; equipment: Equipment[] }) => {
 	return (
-		<div className='border-primary divide-primary h-fit divide-y rounded-xl border bg-black/40 backdrop-blur-xl'>
+		<div className='h-fit divide-y divide-primary rounded-xl border border-primary bg-black/40 backdrop-blur-xl'>
 			<div className='center-text p-3 text-center text-sm text-primary-dark'>{name}</div>
 			{equipment.map((equipment) => (
 				<HeroEquipment key={equipment.name} equipment={equipment} />

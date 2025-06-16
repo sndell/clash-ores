b117d5a5677cfe8ffa7c6a33c012cf4084adadb1
sndell <dev.besu@gmail.com>
2025-06-16 21:39:13 +0200

    fix: change strings saying "weekly" to "monthly"

--- src/components/ui/LandingHeader.tsx
+++ src/components/ui/LandingHeader.tsx
@@ -1,10 +1,10 @@
 export const LandingHeader = () => {
   return (
-    <div className='flex items-center'>
-      <div className='max-w-2xl space-y-2 px-6 text-center'>
+    <div className="flex items-center">
+      <div className="max-w-2xl space-y-2 px-6 text-center">
         <h1>Clash of Clans Equipment Calculators</h1>
-        <p className='text-sm text-primary-dark'>
-          Calculate how many ores you need to max all equipment, a single equipment or caluclate weeklu ore gain.
+        <p className="text-sm text-primary-dark">
+          Calculate how many ores you need to max all equipment, a single equipment or caluclate monthly ore gain.
         </p>
       </div>
     </div>

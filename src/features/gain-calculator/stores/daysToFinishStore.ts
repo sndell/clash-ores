67bf22053dc4b4e08185b76425996af36874fb1c
sndell <dev.besu@gmail.com>
2025-06-17 21:08:26 +0200

    fix: remove persist for daysToFinishStore

--- src/features/gain-calculator/stores/daysToFinishStore.ts
+++ src/features/gain-calculator/stores/daysToFinishStore.ts
@@ -3,15 +3,10 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-export const useDaysToFinishStore = create<DaysToFinishStore>()(
-  persist(
-    (set, get) => ({
-      showDaysToFinish: false,
-      toggleShowDaysToFinish: (value?: boolean) => {
-        set({ showDaysToFinish: value ?? !get().showDaysToFinish });
-        navigator.vibrate(20);
-      },
-    }),
-    { name: "days-to-finish" }
-  )
-);
+export const useDaysToFinishStore = create<DaysToFinishStore>()((set, get) => ({
+  showDaysToFinish: false,
+  toggleShowDaysToFinish: (value?: boolean) => {
+    set({ showDaysToFinish: value ?? !get().showDaysToFinish });
+    navigator.vibrate(20);
+  },
+}));

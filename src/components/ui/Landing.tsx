3516044bf52ea33a87ca5964e94f386e5f8afa0b
sndell <dev.besu@gmail.com>
2026-01-25 11:29:17 +0100

    chore: update Next.js to version 15.5.9 and add LandingFooter component to Landing

--- src/components/ui/Landing.tsx
+++ src/components/ui/Landing.tsx
@@ -1,11 +1,13 @@
 import { LandingHeader } from './LandingHeader';
 import { LandingNav } from './LandingNav';
+import { LandingFooter } from './LandingFooter';
 
 export const Landing = () => {
   return (
     <div className='background-pattern grid h-dvh grid-rows-[1fr_auto_1fr] justify-center'>
       <LandingHeader />
       <LandingNav />
+      <LandingFooter />
     </div>
   );
 };
---
+++ src/components/ui/LandingFooter.tsx
@@ -0,0 +1,8 @@
+export const LandingFooter = () => {
+  return (
+    <div className="flex items-center flex-col justify-center">
+      <span className="text-primary-dark text-sm">New & Improved website available at:</span>
+      <a href="https://www.clashify.app">www.clashify.app</a>
+    </div>
+  );
+};

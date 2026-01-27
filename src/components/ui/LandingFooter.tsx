b56356a1d6d2efc3be9952c47dcff1d11e59dd9d
sndell <dev.besu@gmail..com>
2026-01-27 20:32:50 +0100

    fix: update text in LandingFooter to reflect new calculator availability

--- src/components/ui/LandingFooter.tsx
+++ src/components/ui/LandingFooter.tsx
@@ -1,7 +1,7 @@
 export const LandingFooter = () => {
   return (
     <div className="flex items-center flex-col justify-center">
-      <span className="text-primary-dark text-sm">New & Improved website available at:</span>
+      <span className="text-primary-dark text-sm">New & Improved calculator available at:</span>
       <a href="https://www.clashify.app">www.clashify.app</a>
     </div>
   );
